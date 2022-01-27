1e0848b48f696d8799610157b98c2dc84fff96ef
Anthony <tonysim01@hotmail.com>
2022-01-27 15:32:47 +0100

    ChangeImgPlanetJS

--- app.js
+++ app.js
@@ -10,6 +10,16 @@ let descript = document.querySelector("#descript");
 let distance = document.querySelector("#distkm");
 let traveltime = document.querySelector("#travtime");
 
+let limoon = document.querySelector("#limoon");
+let limars = document.querySelector("#lititan");
+let lieuropa = document.querySelector("#lieuropa");
+let lititan = document.querySelector("#lititan");
+
+let imgplanetmoon=document.querySelector(".imageplanetmoon");
+let imgplanetmars=document.querySelector(".imageplanetmars");
+let imgplaneteuropa=document.querySelector(".imageplaneteuropa");
+let imgplanettitan=document.querySelector(".imageplanettitan");
+
 
 
 moon.addEventListener('click',() => {
@@ -17,6 +27,12 @@ moon.addEventListener('click',() => {
     descript.textContent = "See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.";
     distance.textContent="384,400 KM";
     traveltime.textContent="3 DAYS";
+    limoon.classList.add("selected");
+    imgplanetmoon.style.display="flex";
+    imgplanetmars.style.display="none";
+    imgplaneteuropa.style.display="none";
+    imgplanettitan.style.display="none";
+
 });
 
 mars.addEventListener('click',() => {
@@ -24,6 +40,12 @@ mars.addEventListener('click',() => {
     descript.textContent = "Don’t forget to pack your hiking boots. You’ll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It’s two and a half times the size of Everest!";
     distance.textContent="225 MIL. KM";
     traveltime.textContent="9 MONTHS";
+    limars.addClassList="selected";
+    imgplanetmoon.style.display="none";
+    imgplanetmars.style.display="flex";
+    imgplaneteuropa.style.display="none";
+    imgplanettitan.style.display="none";
+
 });
 
 europa.addEventListener('click',() => {
@@ -31,6 +53,12 @@ europa.addEventListener('click',() => {
     descript.textContent = "The smallest of the four Galilean moons orbiting Jupiter, Europa is a winter lover’s dream. With an icy surface, it’s perfect for a bit of ice skating, curling, hockey, or simple relaxation in your snug wintery cabin.";
     distance.textContent="628 MIL. KM";
     traveltime.textContent="3 YEARS";
+    lieuropa.addClassList="selected";
+    imgplanetmoon.style.display="none";
+    imgplanetmars.style.display="none";
+    imgplaneteuropa.style.display="flex";
+    imgplanettitan.style.display="none";
+
 });
 
 titan.addEventListener('click',() => {
@@ -38,6 +66,12 @@ titan.addEventListener('click',() => {
     descript.textContent = "The only moon known to have a dense atmosphere other than Earth, Titan is a home away from home (just a few hundred degrees colder!). As a bonus, you get striking views of the Rings of Saturn.";
     distance.textContent="1.6 BIL KM";
     traveltime.textContent="7 YEARS";
+    lititan.addClassList="selected";
+    imgplanetmoon.style.display="none";
+    imgplanetmars.style.display="none";
+    imgplaneteuropa.style.display="none";
+    imgplanettitan.style.display="flex";
+
 });
 
 
