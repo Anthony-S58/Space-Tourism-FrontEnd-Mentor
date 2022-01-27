8025aab53f534dbcde1d14132515b2d1cbc306b4
Anthony <tonysim01@hotmail.com>
2022-01-27 13:58:16 +0100

    addJSChangePlanetName

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
 console.log("test");
+
+let moon = document.querySelector("#moon");
+let mars = document.querySelector("#mars");
+let europa = document.querySelector("#europa");
+let titan = document.querySelector("#titan");
+
+let planet = document.querySelector("#planet");
+
+
+moon.addEventListener('click',() => {
+    planet.textContent = "MOON";
+});
+
+mars.addEventListener('click',() => {
+    planet.textContent = "MARS";
+});
+
+europa.addEventListener('click',() => {
+    planet.textContent = "EUROPA";
+});
+
+titan.addEventListener('click',() => {
+    planet.textContent = "TITAN";
+});
+
+
