bce20ab6539702a4247bd8a011d92828236ba432
Anthony <tonysim01@hotmail.com>
2022-01-28 14:38:12 +0100

    prepareJsCrew

---
+++ crewjs.js
@@ -0,0 +1,16 @@
+console.log("hello");
+
+let bulle1 = document.querySelector(".bulle1");
+let bulle2 = document.querySelector(".bulle2");
+let bulle3 = document.querySelector(".bulle3");
+let bulle4 = document.querySelector(".bulle4");
+
+let doug = document.querySelector(".doug");
+let mark = document.querySelector(".mark");
+let victor = document.querySelector(".victor");
+let anousheh = document.querySelector(".anousheh");
+
+let titre = document.querySelector("#titre");
+let name = document.querySelector("#titre");
+let biography = document.querySelector("#titre");
+
