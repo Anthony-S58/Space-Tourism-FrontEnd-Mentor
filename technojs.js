31fea75953ca03534ccdc01fa7ff3d7f227bf684
Anthony <tonysim01@hotmail.com>
2022-01-28 16:16:48 +0100

    prepareTechnoJS

---
+++ technojs.js
@@ -0,0 +1,12 @@
+console.log('test');
+
+let bulle1 = document.querySelector(".bulle1");
+let bulle2 = document.querySelector(".bulle2");
+let bulle3 = document.querySelector(".bulle3");
+
+let launch = document.querySelector(".launch");
+let spaceport = document.querySelector(".spaceport");
+let capsule = document.querySelector(".capsule");
+
+let tools = document.querySelector("#tools");
+let toolsdescript = document.querySelector("#toolsdescript");
